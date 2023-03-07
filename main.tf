bcbd489ae54932fbd89a6dd95aebf30418229e27
Joaquin Graziano <jgraziano@altec.com.ar>
2023-03-07 10:16:41 -0300

    main.tf

--- main.tf
+++ main.tf
@@ -1,39 +1,19 @@
 provider "aws" {
-  alias                         = "localstack"
-  region                        = "us-east-1"
-  access_key                    = "test"
-  secret_key                    = "test"
-  skip_credentials_validation  = true
-  skip_metadata_api_check      = true
-  skip_requesting_account_id   = true
+
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+  region                      = "us-east-1"
+
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
   endpoints {
-    ec2             = "http://localhost:4566/"
-    s3              = "http://localhost:4566/"
-    sts             = "http://localhost:4566/"
-    iam             = "http://localhost:4566/"
-    cloudformation  = "http://localhost:4566/"
-    cloudwatch      = "http://localhost:4566/"
-    eks             = "http://localhost:4566/"
-    apigateway      = "http://localhost:4566/"
-    lambda          = "http://localhost:4566/"
-    ssm             = "http://localhost:4566/"
-  }
-} 
-
-
-resource "aws_vpc" "eks_vpc" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "eks-vpc"
+    s3             = "http://s3.localhost.localstack.cloud:4566"
   }
 }
 
-resource "aws_subnet" "eks_subnet_private" {
-  vpc_id     = aws_vpc.eks_vpc.id
-  cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Name = "eks-subnet-private"
-  }
+resource "aws_s3_bucket" "test-bucket" {
+  bucket = "my-bucket"
 }
