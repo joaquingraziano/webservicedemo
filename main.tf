c66696eec60a40490f2d34be404dab092a9fc887
Joaquin Graziano <jgraziano@altec.com.ar>
2023-03-07 10:08:25 -0300

    main.tf + var

--- main.tf
+++ main.tf
@@ -1,18 +1,32 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
+    required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "~> 4.0"
     }
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+    region = "us-east-1"
+    access_key = "my-access-key"
+    secret_key = "my-secret-key"
 }
 
-# Create a VPC
-resource "aws_vpc" "example" {
+resource "aws_vpc" "eks_vpc" {
   cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "eks-vpc"
+  }
+}
+
+resource "aws_subnet" "eks_subnet_private" {
+  vpc_id     = aws_vpc.eks_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "eks-subnet-private"
+  }
 }
