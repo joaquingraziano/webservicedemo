7a36f2b817cf5cb25201dab51a00f281869e7b15
Joaquin Graziano <jgraziano@altec.com.ar>
2023-03-07 10:12:35 -0300

    main.tf + var

--- main.tf
+++ main.tf
@@ -1,10 +1,25 @@
-
-# Configure the AWS Provider
 provider "aws" {
-    region = "us-east-1"
-    access_key = "my-access-key"
-    secret_key = "my-secret-key"
-}
+  alias                         = "localstack"
+  region                        = "us-east-1"
+  access_key                    = "test"
+  secret_key                    = "test"
+  skip_credentials_validation  = true
+  skip_metadata_api_check      = true
+  skip_requesting_account_id   = true
+  endpoints {
+    ec2             = "http://localhost:4566/"
+    s3              = "http://localhost:4566/"
+    sts             = "http://localhost:4566/"
+    iam             = "http://localhost:4566/"
+    cloudformation  = "http://localhost:4566/"
+    cloudwatch      = "http://localhost:4566/"
+    eks             = "http://localhost:4566/"
+    apigateway      = "http://localhost:4566/"
+    lambda          = "http://localhost:4566/"
+    ssm             = "http://localhost:4566/"
+  }
+} 
+
 
 resource "aws_vpc" "eks_vpc" {
   cidr_block = "10.0.0.0/16"
