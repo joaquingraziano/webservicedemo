12dbf54687589f2bf7c8d83e82e5fd65029d3bf7
Joaquin Graziano <jgraziano@altec.com.ar>
2023-03-07 10:50:03 -0300

    agrego provider + vpc

--- providers.tf
+++ providers.tf
@@ -1,8 +1,10 @@
 provider "aws" {
+
   access_key                  = "mock_access_key"
-  secret_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
   region                      = "us-east-1"
-  s3_force_path_style         = false
+
+  s3_use_path_style           = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
@@ -32,4 +34,4 @@ provider "aws" {
     stepfunctions  = "http://localhost:4566"
     sts            = "http://localhost:4566"
   }
-} 
+}
