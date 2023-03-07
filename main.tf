0e5c4010867504f1c05873d4bf62beeae733ad3e
Joaquin Graziano <jgraziano@altec.com.ar>
2023-03-07 10:52:08 -0300

    agrego provider + vpc

--- main.tf
+++ main.tf
@@ -1,52 +1,8 @@
 
+
 //se creara
 
-resource "aws_vpc" "eks_vpc" {
-  cidr_block = "10.0.0.0/16"
 
-  tags = {
-    Name = "eks-vpc"
-  }
-}
-
-resource "aws_subnet" "eks_subnet_private" {
-  vpc_id     = aws_vpc.eks_vpc.id
-  cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Name = "eks-subnet-private"
-  }
-}
-
-resource "aws_subnet" "eks_subnet_public" {
-  vpc_id     = aws_vpc.eks_vpc.id
-  cidr_block = "10.0.2.0/24"
-
-  tags = {
-    Name = "eks-subnet-public"
-  }
-}
-
-resource "aws_internet_gateway" "eks_igw" {
-  vpc_id = aws_vpc.eks_vpc.id
-
-  tags = {
-    Name = "eks-igw"
-  }
-}
-
-resource "aws_route_table" "eks_private_route_table" {
-  vpc_id = aws_vpc.eks_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.eks_igw.id
-  }
-
-  tags = {
-    Name = "eks-private-route-table"
-  }
-}
 
 resource "aws_security_group" "eks_worker_sg" {
   name_prefix = "eks-worker-"
