bd9586aa06b54a42cfb695395d03bb2448e3fb14
Joaquin Graziano <jgraziano@altec.com.ar>
2023-03-07 09:36:55 -0300

    main.tf

--- main.tf
+++ main.tf
@@ -14,5 +14,9 @@ resource "docker_container" "ubuntu" {
     name = "ubuntu1"
     image = "ubuntu:18.04"
     command = [ "/bin/bash","-c","while true; do sleep 60000; done"]
+
+    provisioner "local-exec" {
+        command = "echo ${docker_container.ubuntu.network_data[0].ip_address} >> inventario.txt"
+    }
 }
 
